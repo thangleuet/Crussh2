b69f4878f50e3a64372ff4bf4bf2af4909693da8
Thangleuet <thangleuet@gmail.com>
2021-09-17 14:09:28 +0700

    new1

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    introTitle: 'Helu Cậu !!!',
+    introDesc: `Tớ có điều muốn hỏi cậu nhớ phải trả lời thật lòng nhaaaa!!!`,
+    btnIntro: 'Oke',
+    title: 'Có phải Cậu đang thích tớ đúng hong?',
+    desc: 'Nếu cậu không trả lời mà thoát ra tức là muốn làm ny tới rùi đó :v ',
+    btnYes: 'Iu nhìu nhìuuuuu',
+    btnNo: 'Đừng có mà mơ :3333',
+    question:'Trên thế giới hơn 7 tỉ người mà cậu chọn tớ là đúng đó <3',
+    btnReply: 'Gửi cho tớ <3',
+    reply: 'Yêu thì yêu mà không yêu thì vẫn phải yêu <33333333',
+    mess: 'Tớ biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
+    messDesc: 'Hết dịch tớ sẽ dẫn cậu đi chơi nha <3 <3. Rồi ngày nào, giờ nào tớ cũng inbox cho cậu =))',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/tatthang.le.397/'
 }
